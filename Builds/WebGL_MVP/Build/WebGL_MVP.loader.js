7944beb0c3bea167c0ec28635e40ca1a56a9cbfa
Muhammet <mayibey@gmail.com>
2026-05-22 01:43:13 +0300

    Deploy: Faz 35.47-48 build (karsilama metni + modal hizalama)

--- Builds/WebGL_MVP/Build/WebGL_MVP.loader.js
+++ Builds/WebGL_MVP/Build/WebGL_MVP.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 73653677,
+    wasmFileSize: 73654108,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
